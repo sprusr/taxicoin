7e3aaeb53065bcac4c1672de29da9b2c823e1dcd
sprusr <scott@spru.sr>
2018-04-25 18:13:20 +0100

    Add rinkeby to truffle networks list

--- truffle.js
+++ truffle.js
@@ -3,7 +3,13 @@ module.exports = {
     development: {
       host: "localhost",
       port: 7545,
-      network_id: "5777", // Match any network id
+      network_id: "5777", // ganache
+      gas: 6000000
+    },
+    rinkeby: {
+      host: "localhost",
+      port: 8545,
+      network_id: "4", // Match any network id
       gas: 6000000
     }
   },
