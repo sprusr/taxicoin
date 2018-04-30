6345acb1b234a82bdcb546f2eb2f0ed653d81fb1
sprusr <scott@spru.sr>
2018-04-30 11:55:47 +0100

    Deploy

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var serviceWorkerOption = {"assets":["/static/js/vendor.2735e20d60abb2f58c6d.js","/static/js/app.6ec119616c72f56d6d51.js","/static/js/manifest.fd46343eeb86e57f89fa.js","/static/css/app.065c49666fc6d46ecbbef6e1051062d1.css"]};
+var serviceWorkerOption = {"assets":["/static/js/vendor.18bf665e2f1542d2f4dc.js","/static/js/app.f9b6517b83f795618e85.js","/static/js/manifest.bc742a02013abe4b9576.js","/static/css/app.4b181e73f97e6f94a332315500cea92f.css"]};
         
         !function(n){function r(e){if(t[e])return t[e].exports;var o=t[e]={i:e,l:!1,exports:{}};return n[e].call(o.exports,o,o.exports,r),o.l=!0,o.exports}var t={};r.m=n,r.c=t,r.i=function(n){return n},r.d=function(n,t,e){r.o(n,t)||Object.defineProperty(n,t,{configurable:!1,enumerable:!0,get:e})},r.n=function(n){var t=n&&n.__esModule?function(){return n.default}:function(){return n};return r.d(t,"a",t),t},r.o=function(n,r){return Object.prototype.hasOwnProperty.call(n,r)},r.p="",r(r.s="3gGW")}({"3gGW":function(n,r){}});
 //# sourceMappingURL=sw.js.map
